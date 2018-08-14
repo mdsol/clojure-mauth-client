917ea24c8f8f2cccd65db59aa8252d508cdd3d88
Pramod Somashekar <psomashekar@mdsol.com>
2018-08-13 21:33:57 -0400

    bumping version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-mauth-client "1.0.1"
+(defproject clojure-mauth-client "1.0.2"
   :description "Clojure Mauth Client"
   :url "https://github.com/mdsol/clojure-mauth-client"
   :license {:name "MIT"
