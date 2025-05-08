5db6e8c8c7a8512c9c480d7b1c88dc059bee9b41
Abhijit Kumbhar <abkumbhar@mdsol.com>
2025-05-08 21:16:10 +0530

    Version 2.0.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-mauth-client "2.0.8-SNAPSHOT"
+(defproject clojure-mauth-client "2.0.8"
   :description "Clojure Mauth Client"
   :url "https://github.com/mdsol/clojure-mauth-client"
   :license {:name "MIT"
