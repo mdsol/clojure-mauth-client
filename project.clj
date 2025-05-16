6e33cfd42ebae6cd0793f0c31697d815dfc9c3e3
Colin P. Hill <colin.hill@3ds.com>
2025-05-16 10:25:19 -0400

    Add alpha1 qualifier to version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-mauth-client "3.0.0-SNAPSHOT"
+(defproject clojure-mauth-client "3.0.0-alpha1-SNAPSHOT"
   :description "Clojure Mauth Client"
   :url "https://github.com/mdsol/clojure-mauth-client"
   :license {:name "MIT"
